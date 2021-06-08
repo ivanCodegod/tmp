1a5933dfa4d83c6b27d1afd0203a164240deaab3
Ivan <bozkovana@gmail.com>
2021-06-08 20:36:40 +0300

    make 2new files to have actions with matriz

--- task1.py
+++ task1.py
@@ -1,13 +1,16 @@
-a, b, c, d = [int(input()) for i in range(4)]
-r = 'ZOMBIE'
+from task2 import description, action
 
-if d != 0 and a == 618 and ((b * c) / d).is_integer() and 0 <= ((b * c) / d) <= 10 ** 9:
-    r = ((b * c) / d)
-elif c != 0 and b == 618 and ((a * d) / c).is_integer() and 0 <= ((a * d) / c) <= 10 ** 9:
-    r = ((a * d) / c)
-elif b != 0 and c == 618 and ((a * d) / b).is_integer() and 0 <= ((a * d) / b) <= 10 ** 9:
-    r = ((a * d) / b)
-elif a != 0 and d == 618 and ((b * c) / a).is_integer() and 0 <= ((b * c) / a) <= 10 ** 9:
-    r = ((b * c) / a)
+listt = []
 
-print(r)
+print("Напиши 'help' чтоб увидеть список доступных команд")
+while True:
+    inp = input()
+    if inp == 'q':
+        print("Программа остановленна")
+        break
+    elif inp == 'help':
+        description()
+    elif inp == 'action':
+        action(inp)
+    else:
+        description()
--- task2.py
+++ task2.py
@@ -1,16 +1,71 @@
-def is_merge(s, part1, part2):
-    sting = part1 + part2
-    a = list(part1)
-    b = list(part2)
+def commands():
+    print(
+        "(action)Введите:\n\trow - для замены двух РЯДОВ\n\tcolumn - для замены двух СТОЛБЦОВ\n\tmin - для нахождения "
+        "МИНИМАЛЬНОГО ЧИСЛА\n\tmax - для нахождения максимального числа\n\tq - для выхода из action")
 
-    if sorted(s) != sorted(sting): return False
-    for ind, i in enumerate(list(s)):
-        if i != s[-1] and i in a and i > a.index(s[ind]):
-            return False
-        if i != s[-1] and i in b and i > b.index(s[ind]):
-            return False
 
-    return True if len(sting) == len(s) else False
+def description():
+    print(
+        "--------------------------------------------------------\nДоступные команды:\n\t q - завершение "
+        "программы\n\t help "
+        "- Список доступных программ\n\t action - свап двух РЯДОВ или двух СТОЛБЦОВ матрицы или поиск МИНИМАЛЬНОГО "
+        "или МАКСИМАЛЬНОГО чисел "
+        "\n--------------------------------------------------------")
 
-print("Halelya")
-print(is_merge('codewars', 'code', 'wasr'))
+
+def action(tmp):
+    a, b = map(int, input("Введите размер квадратной матрицы (через пробел): ").split())
+    listt = []
+
+    print("Введите элементы матрицы в зависимости от ее размера (через пробел):\n\tНапример, матрица 2x2 имеет "
+          "вид:\n\t\t1 2\n\t\t2 3")
+    for i in range(a):
+        numbers = list(map(int, input().split()))
+        listt.append(numbers)
+    commands()
+    while True:
+        how = input()
+        if how == 'q':
+            return
+        elif how == 'help':
+            commands()
+        elif how == 'row':
+            try:
+                c, d = map(int, input("Введите номера двух РЯДОВ для их обмена(нумерация от 1): ").split())
+                print("Ваш результат: ")
+                listt[c - 1], listt[d - 1] = listt[d - 1], listt[c - 1]
+            except ValueError:
+                print("Не возможно создать матрицу :(")
+                commands()
+            for numb in listt:
+                print(*numb, end='\n')
+            print("Напиши 'help' чтоб увидеть список доступных команд")
+
+        elif how == 'column':
+            e, b = map(int, input("Введите номера двух СТОЛБЦОВ для их обмена(нумерация от 1): ").split())
+            print("Ваш результат: ")
+
+            for j in range(a):
+                listt[j][e - 1], listt[j][b - 1] = listt[j][b - 1], listt[j][e - 1]
+            for numb in listt:
+                print(*numb, end='\n')
+            print("Напиши 'help' чтоб увидеть список доступных команд")
+        elif how == 'min':
+            min = listt[0][0]
+            for i in listt:
+                for j in i:
+                    if j < min:
+                        min = j
+            print(f"{min} - минимальное число в заданной матрице")
+            print("Напиши 'help' чтоб увидеть список доступных команд")
+        elif how == 'max':
+            max = listt[0][0]
+            for i in listt:
+                for j in i:
+                    if j > max:
+                        max = j
+            print(f"{max} - максимальное число в заданной матрице")
+            print("Напиши 'help' чтоб увидеть список доступных команд")
+        else:
+            commands()
+
